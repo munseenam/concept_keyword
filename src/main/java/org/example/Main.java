f05d44f411a677ec15faaf6b237a0702b5baaea8
munseenam <117824519+munseenam@users.noreply.github.com>
2022-12-28 16:15:48 +0530

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        checkKeyword word=new  checkKeyword();
+        word.checkword("0");
+
+
+    }
+}
